f160ddeba2dcded97c87b1b3d8c86cb50436dfee
Izaak Khaliq <(none)>
2016-06-02 20:46:08 +0800

    Add initial spec for thailand.servishero.com site

--- test/specs/example.spec.js
+++ test/specs/example.spec.js
@@ -2,8 +2,8 @@ var assert = require('assert');
 
 describe('webdriver.io page', function() {
     it('should have the right title - the fancy generator way', function () {
-        browser.url('http://webdriver.io');
+        browser.url('http://thailand.servishero.com/th/en/bangkok');
         var title = browser.getTitle();
-        assert.equal(title, 'WebdriverIO - Selenium 2.0 javascript bindings for nodejs');
+        assert.equal(title, 'Hire Trusted Service Professionals in Bangkok | ServisHero');
     });
 });
